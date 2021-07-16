4a80c824995346a1903308622fd4c5543c430c08
nirajk122 <nirajkumarjena@gmail.com>
2021-07-16 22:47:14 +0530

    4th

--- src/index.js
+++ src/index.js
@@ -6,35 +6,46 @@ import './index.css';
 //always return JSX
 //css
 //set up variable
-const firstBook = {
+const books = [
+{id: 1,
   img: 'https://images-eu.ssl-images-amazon.com/images/I/51K8xEvReAS._AC_SX184_.jpg',
   title: 'The Three Khans: And the Emergence of New IndiaThe Three Khans: And the Emergence of New IndiaThe Three Khans: And the Emergence of New…',
   author: 'Kaveree Bamzai'
-}
-const secondBook = {
+},
+{id: 2,
   img: 'https://images-na.ssl-images-amazon.com/images/I/51NHKZC1a0L._SX324_BO1,204,203,200_.jpg',
   title:'Grandmas Bag of Stories',
   author: 'sudha murthy'
+},
+{id: 3,
+  img: 'https://images-eu.ssl-images-amazon.com/images/I/61n26kH1HES._SY264_BO1,204,203,200_QL40_FMwebp_.jpg',
+  title: 'The Upside-Down King: Unusual Tales about Rama and Krishna',
+  author: 'sudha murthy'
 }
+];
+const names = ['niraj', 'ilan', 'jena']
+const newName = names.map((name)=>{
+  return <h1>{name}</h1>
+})
+console.log(newName);
 function BookList(){
   return <section className="booklist">
-    <Book 
-    img={firstBook.img} 
-    title={firstBook.title} 
-    author={firstBook.author}/>
-    <Book 
-    img={secondBook.img} 
-    title={secondBook.title} 
-    author={secondBook.author}/>
-  </section>;
+    {books.map((book, index)=>{
+    return (
+      <Book key={book.id} {...book}></Book>
+    );
+  })}</section>;
 }
 //use of props
-const Book = ({ img, title, author}) => {
-  // const {img, title, author} = props
+const Book = ({ img, title, author }) => {
+  // console.log(props);
+  // const {img, title, author} = props;
+  
   return <article className='book'>
     <img src={img} alt=""/>
     <h1>{title}</h1>
     <h3>{author}</h3>
+    
   </article>
 }
 
