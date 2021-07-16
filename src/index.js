66d18f08cc455e9e312eb98669c6dad99a53b008
nirajk122 <nirajkumarjena@gmail.com>
2021-07-16 19:01:59 +0530

    secondary commit

--- src/index.js
+++ src/index.js
@@ -18,13 +18,10 @@ function BookList(){
 }
 const Book = () => {
   return <article className='book'>
-    <Image></Image>
-    <Title></Title>
-    <Author/>
+    <img src="https://images-eu.ssl-images-amazon.com/images/I/51K8xEvReAS._AC_SX184_.jpg" alt=""/>
+    <h1>The Three Khans: And the Emergence of New IndiaThe Three Khans: And the Emergence of New IndiaThe Three Khans: And the Emergence of New…</h1>
+    <h3 style={{color: '#617d98',fontSize: '0.75rem', marginTop: '0.25rem'}}>Kaveree Bamzai</h3>
   </article>
 }
-const Image = () => <img src="https://images-eu.ssl-images-amazon.com/images/I/51K8xEvReAS._AC_SX184_.jpg" alt=""/>
 
-const Title = () => <h1>The Three Khans: And the Emergence of New IndiaThe Three Khans: And the Emergence of New IndiaThe Three Khans: And the Emergence of New…</h1>
-const Author = () => <h3 style={{color: '#617d98',fontSize: '0.75rem', marginTop: '0.25rem'}}>Kaveree Bamzai</h3>
 ReactDOM.render(<BookList/>, document.getElementById('root'));
