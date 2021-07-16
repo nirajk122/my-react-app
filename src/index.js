ed898faae781682afe9c66ec3b45ec35a559ab90
nirajk122 <nirajkumarjena@gmail.com>
2021-07-16 18:44:38 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//stateless functional component
+//always return JSX
+//css
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function BookList(){
+  return <section className="booklist">
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+    <Book/>
+  </section>;
+}
+const Book = () => {
+  return <article className='book'>
+    <Image></Image>
+    <Title></Title>
+    <Author/>
+  </article>
+}
+const Image = () => <img src="https://images-eu.ssl-images-amazon.com/images/I/51K8xEvReAS._AC_SX184_.jpg" alt=""/>
+
+const Title = () => <h1>The Three Khans: And the Emergence of New IndiaThe Three Khans: And the Emergence of New IndiaThe Three Khans: And the Emergence of New…</h1>
+const Author = () => <h3 style={{color: '#617d98',fontSize: '0.75rem', marginTop: '0.25rem'}}>Kaveree Bamzai</h3>
+ReactDOM.render(<BookList/>, document.getElementById('root'));
